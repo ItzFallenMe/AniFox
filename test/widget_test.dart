353b6463ab092c765876e7cfa7683da03deff172
(Itz_Me) <vedanimefan@gmail.com>
2026-07-29 06:11:19 +0530

    Delete test directory
    Signed-off-by: (Itz_Me) <vedanimefan@gmail.com>

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:anifox/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const AniFox());
-    expect(find.byType(AniFox), findsOneWidget);
-  });
-}
